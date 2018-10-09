4b3962722892ed40897e0aed685399d0d151e16c
Thomas <Thomas@dhcp-10-248-132-125.eduroam.wireless.private.cam.ac.uk>
2018-10-09 13:16:09 +0100

    continued working

--- SA.py
+++ SA.py
@@ -1,1 +1,8 @@
 import numpy
+
+# define function that is time derivative of voltage: f = dV/dt
+def f(V, x, y):
+    # set the relevant constants
+    I_e, g_K, g_Na, V_K, V_Na, V_l = x
+    # set the 
+    n, m, h = y
