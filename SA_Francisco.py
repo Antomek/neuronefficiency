aab579ba79a06fb77f2a3cc6419ee660134e79d3
Thomas <Thomas@dhcp-10-248-129-13.eduroam.wireless.private.cam.ac.uk>
2018-10-11 14:15:05 +0100

    Francisco's attempt to help me out; note high error tolerances.

---
+++ SA_Francisco.py
@@ -0,0 +1,59 @@
+import numpy as np
+import scipy as sp
+import scipy.integrate as sp_int
+import matplotlib.pyplot as plt
+import matplotlib
+
+# define function that is time derivative of voltage: f = dV/dt
+def f(t, y):
+    # print (t,y, "kek")
+    # set the relevant constants
+    I_e, C_m, g_K, g_Na, g_l, V_K, V_Na, V_l = x
+    # set the variables that are to be integrated
+    V, n, m, h = y
+    # define DV/dt ('_dot' denotes time differentiation)
+    V_dot = 1/C_m * (I_e - (g_K * (n**4) * (V - V_K) + g_Na * (m**3)*h) *
+        (V - V_Na) + g_l * (V - V_l))
+
+    # equations governing opening/closing rates.
+    a_n = 0.01 * (V + 10) / ( np.exp((V+10)/10) - 1)
+    b_n = 0.125 * np.exp(V/80)
+    a_m = 0.1 * (V + 25) / (np.exp((V + 25)/10) - 1)
+    b_m = 4 * np.exp(V / 18)
+    a_h = 0.07 * np.exp(V / 20)
+    b_h = 1 / (np.exp((V + 30)/10) + 1)
+
+    # enter the equations controlling the gating variables.
+    # 'g_1' is dn/dt, 'g_2' is dm/dt, 'g_3' is dh/dt
+    g_1 = a_n * (1 - n) - b_n*n
+    g_2 = a_m * (1 - m) - b_m*m
+    g_3 = a_h * (1 - h) - b_h*h
+    # print("kek2")
+    # since w = [V, n, m, h] we return [V_dot, n_dot, m_dot, h_dot]
+    return [V_dot, g_1, g_2, g_3]
+
+# print("hello pig")
+# enter the values of the constants. Values taken from Table 3 in `Membrane Current In Nerve`
+# values are entered like: conts = [I_e, C_m, g_K, g_Na, g_l, V_K, V_Na, V_l]
+conts = [1.0, 1.0, 36, 120, 0.3, 12, -115, -10.613]
+# enter intial values for V, n, m, h
+V_0 = 1
+n_0 = 0
+m_0 = 0
+h_0 = 0
+y_0 = [V_0, n_0, m_0, h_0]
+
+# print("pig initialised")
+# create timescale. t_interval is the time interval in which to calculate the solution.
+# t_points are the points at which the solution is stored.
+t_interval = (0.0, 10.0)
+numpoints = 1000
+t_points = np.linspace(t_interval[0], t_interval[1], numpoints)
+
+x = conts
+# solve coupled ODEs with scipy's solver
+# print("about to solve pig")
+soln = sp_int.solve_ivp(f, t_interval, y_0, 'RK45', t_points, atol=0.1, rtol=0.01)
+
+plt.plot(soln.t, soln.y[0, :])
+plt.show()
