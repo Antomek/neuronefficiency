12ae75d8fff922b6d1ef0de7c476fe9a381bc22c
Thomas <Thomas@dhcp-10-248-128-4.eduroam.wireless.private.cam.ac.uk>
2018-11-08 10:58:49 +0000

    Added calcium pool equations to model

--- RG.py
+++ RG.py
@@ -27,8 +27,8 @@ E_l = -65
 def exp(V, A, B, C, V_0): return A * np.exp(B * (V - 10 - V_0)) + C
 def sig(V, A, B, V_0): return A / (1 + np.exp(B * (V - 10 - V_0)))
 def lin(V, A, B, V_0): return A * (V - V_0) / (np.exp(B * (V - 10 - V_0)) - 1)
-def KC_alpha(V, A, B, C, con): return A / (1 + C * np.exp(B * (V - 10) / con) # con stands for Ca2+ concentration
-def KC_beta(V, A, B, C, con): return A / (1 + con / (C * np.exp(B * (V - 10)))
+def KC_alpha(V, A, B, C, con): return A / (1 + C * np.exp(B * (V - 10) / con)) # con stands for Ca2+ concentration
+def KC_beta(V, A, B, C, con): return A / (1 + con / (C * np.exp(B * (V - 10))))
 
 # Parameter values of voltage-gated channels (cf. Table 1 in paper). 'am' is alpha fn for activation function m, 'bh' is beta function for inactivation function h etc.
 A_NaF_am, B_NaF_am, V0_NaF_am = [7.5, 0.081, -39]
@@ -69,13 +69,13 @@ def am_H(V):   return exp(V, A_H_am, B_H_am, 0, V0_H_am)
 def bm_H(V):    return exp(V, A_H_bm, B_H_bm, 0, V0_H_bm)
 def taum_KA(V): return exp(V, A_KA_taum, B_KA_taum, C_KA_taum, V0_KA_taum)
 def minf_KA(V): return sig(V, A_KA_minf, B_KA_minf, V0_KA_minf)
-def tauh_KA(V): return 10.8 + 0.03*V + 1 / (57.9 * np.exp(0.127*V) + 0.000134*np.exp(-0.059*V))
+def tauh_KA(V): return 10.8 + 0.03*(V - 10) + 1 / (57.9 * np.exp(0.127*(V - 10)) + 0.000134*np.exp(-0.059*(V - 10)))
 def hinf_KA(V): return sig(V, A_KA_hinf, B_KA_hinf, V0_KA_hinf)
-def am_KC(V):   return KC_alpha(V, A_KC_am, B_KC_am, C_KC_am, #insert conc)
-def bm_KC(V):   return KC_beta(V, A_KC_bm, B_KC_bm, C_KC_bm)
+def am_KC(V, con):   return KC_alpha(V, A_KC_am, B_KC_am, C_KC_am, con)
+def bm_KC(V, con):   return KC_beta(V, A_KC_bm, B_KC_bm, C_KC_bm, con)
 
 # define the currents in the model
-def I_NaF(V, m, h): return g_Na * (m**3) * h * (V - E_NaF)
+def I_NaF(V, m, h): return g_NaF * (m**3) * h * (V - E_NaF)
 def I_Kdr(V, m, h): return g_Kdr * (m**4) * h * (V - E_Kdr)
 def I_CaL(V, m, h): return g_CaL * (m**2) * h * (V - E_CaL)
 def I_H(V, m): return g_H * m * (V - E_H)
@@ -91,7 +91,7 @@ def f(t, y):
     else:
         I_e = 0
     # set the variables that are to be integrated
-    V, m_NaF, h_NaF, m_Kdr, h_Kdr, m_CaL, h_CaL, m_H, h_H, m_KA, h_Ka, m_KC = y
+    V, m_NaF, h_NaF, m_Kdr, h_Kdr, m_CaL, h_CaL, m_H, m_KA, h_Ka, m_KC, CA_con = y
     # define DV/dt ('_dot' denotes time differentiation)
     V_dot = 1/C_m * (I_e - (I_NaF(V, m_NaF, h_NaF) + I_Kdr(V, m_Kdr, h_Kdr)
             + I_CaL(V, m_CaL, h_CaL) + I_H(V, m_H) + I_KA(V, m_KA, h_KA)
@@ -106,21 +106,24 @@ def f(t, y):
     m_H_dot = am_H(V) * (1 - m_H) - bm_H(V) * h_H
     m_KA_dot = (minf_KA(V) - m_KA) / taum_KA(V)
     h_KA_dot = (hinf_KA(V) - h_KA) / tauh_KA(V)
-    m_KC_dot = am_KC(V) * (1 - m_KC) - bm_KC(V) * m_KC
-    return [V_dot, m_NaF_dot, h_NaF_dot, m_Kdr_dot, h_Kdr_dot, m_CaL_dot, h_CaL_dot, m_H_dot, m_KA_dot, h_KA_dot, m_KC_dot]
+    m_KC_dot = am_KC(V, CA_con) * (1 - m_KC) - bm_KC(V, CA_con) * m_KC
+    CA_con_dot = (I_CaL(V, m_CaL, h_CaL) / (2*96,494*0.084*(10**(-6))) ) - (CA_con - 75.5*(10**(-6))) / 10
+    return [V_dot, m_NaF_dot, h_NaF_dot, m_Kdr_dot, h_Kdr_dot, m_CaL_dot, h_CaL_dot, m_H_dot, m_KA_dot, h_KA_dot, m_KC_dot, CA_con_dot]
 
-# enter intial values for V, m's, h's etc. (using initianlisation m_0 = m_inf(V_0) )
-V_0 = -65
+# enter intial values for V, m's, h's etc. (using initialisation m_0 = m_inf(V_0) )
+V_0 = -68
 m_NaF_0 = 1 / (am_NaF(V_0)  + bm_NaF(V_0))
 h_NaF_0 = 1 / (ah_NaF(V_0)  + bh_NaF(V_0))
 m_Kdr_0 = 1 / (am_Kdr(V_0)  + bm_Kdr(V_0))
 h_Kdr_0 = 1 / (ah_Kdr(V_0)  + bh_Kdr(V_0))
 m_CaL_0 = 1 / (am_CaL(V_0)  + bm_CaL(V_0))
 h_CaL_0 = 1 / (ah_CaL(V_0)  + bh_CaL(V_0))
-m_KA_0 = minf_KA(V_0)
-h_KA_0 = hinf_KA(V_0)
-m_KC_0 = 1 / (am_KC(V_0)  + bm_KC(V_0))
-y_0 = [V_0, m_NaF_0, h_NaF_0, m_Kdr_0, h_Kdr_0, m_CaL_0, h_CaL_0, m_KA_0, h_KA_0, m_KC_0]
+m_H_0   = 1 / (am_H(V_0)  + bm_H(V_0))
+m_KA_0  = minf_KA(V_0)
+h_KA_0  = hinf_KA(V_0)
+CA_con_0 = 75.5*(10**(-6))
+m_KC_0  = 1 / (am_KC(V_0, CA_con_0)  + bm_KC(V_0, CA_con_0))
+y_0 = [V_0, m_NaF_0, h_NaF_0, m_Kdr_0, h_Kdr_0, m_CaL_0, h_CaL_0, m_H_0, m_KA_0, h_KA_0, m_KC_0, CA_con_0]
 
 # create timescale. t_interval is the time interval in which to calculate the solution.
 # t_points are the points at which the solution is stored.
