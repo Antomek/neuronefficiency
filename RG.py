98b16fbf33799f6f69b5256fa78be5ea9142f78e
Thomas <Thomas@dhcp-10-248-128-4.eduroam.wireless.private.cam.ac.uk>
2018-11-08 09:53:20 +0000

    Expanded model, working on calcium concentrations

--- RG.py
+++ RG.py
@@ -24,11 +24,11 @@ E_KC = -90
 E_l = -65
 
 # auxiliary equations as defined in paper
-def exp(V, A, B, C, V_0): return A * np.exp(B * (V - V_0)) + C
-def sig(V, A, B, V_0): return A / (1 + np.exp(B * (V - V_0)))
-def lin(V, A, B, V_0): return A * (V - V_0) / (np.exp(B * (V - V_0)) - 1)
-def KC_alpha(V, A, B, C, con): return A / (1 + C * np.exp(B * V) / con) # con stands for Ca2+ concentration
-def KC_beta(V, A, B, C, con): return A / (1 + con / (C * np.exp(B * V)))
+def exp(V, A, B, C, V_0): return A * np.exp(B * (V - 10 - V_0)) + C
+def sig(V, A, B, V_0): return A / (1 + np.exp(B * (V - 10 - V_0)))
+def lin(V, A, B, V_0): return A * (V - V_0) / (np.exp(B * (V - 10 - V_0)) - 1)
+def KC_alpha(V, A, B, C, con): return A / (1 + C * np.exp(B * (V - 10) / con) # con stands for Ca2+ concentration
+def KC_beta(V, A, B, C, con): return A / (1 + con / (C * np.exp(B * (V - 10)))
 
 # Parameter values of voltage-gated channels (cf. Table 1 in paper). 'am' is alpha fn for activation function m, 'bh' is beta function for inactivation function h etc.
 A_NaF_am, B_NaF_am, V0_NaF_am = [7.5, 0.081, -39]
@@ -60,10 +60,10 @@ def bh_NaF(V): return exp(V, A_NaF_bh, B_NaF_bh, 0, V0_NaF_bh)
 def am_Kdr(V): return exp(V, A_Kdr_am, B_Kdr_am, 0, V0_Kdr_am)
 def bm_Kdr(V): return exp(V, A_Kdr_bm, B_Kdr_bm, 0, V0_Kdr_bm)
 def ah_Kdr(V): return exp(V, A_Kdr_ah, B_Kdr_ah, C_Kdr_ah, V0_Kdr_ah)
-def bh_Kdr(V): return sig(V, A_Kdr_bh, B_Kdr_bh, 0, V0_Kdr_bh)
-def am_CaL(V): return sig(V, A_CaL_am, B_CaL_am, 0, V0_CaL_am)
-def bm_CaL(V): return lin(V, A_CaL_bm, B_CaL_bm, 0, V0_CaL_bm)
-def ah_CaL(V): return exp(V, A_CaL_ah, B_CaL_ah, C_CaK_Bh, V0_CaL_ah)
+def bh_Kdr(V): return sig(V, A_Kdr_bh, B_Kdr_bh, V0_Kdr_bh)
+def am_CaL(V): return sig(V, A_CaL_am, B_CaL_am, V0_CaL_am)
+def bm_CaL(V): return lin(V, A_CaL_bm, B_CaL_bm, V0_CaL_bm)
+def ah_CaL(V): return exp(V, A_CaL_ah, B_CaL_ah, C_CaL_bh, V0_CaL_ah)
 def bh_CaL(V): return exp(V, A_CaL_bh, B_CaL_bh, 0, V0_CaL_bh)
 def am_H(V):   return exp(V, A_H_am, B_H_am, 0, V0_H_am)
 def bm_H(V):    return exp(V, A_H_bm, B_H_bm, 0, V0_H_bm)
@@ -71,8 +71,8 @@ def taum_KA(V): return exp(V, A_KA_taum, B_KA_taum, C_KA_taum, V0_KA_taum)
 def minf_KA(V): return sig(V, A_KA_minf, B_KA_minf, V0_KA_minf)
 def tauh_KA(V): return 10.8 + 0.03*V + 1 / (57.9 * np.exp(0.127*V) + 0.000134*np.exp(-0.059*V))
 def hinf_KA(V): return sig(V, A_KA_hinf, B_KA_hinf, V0_KA_hinf)
-def am_KC(V):   return KCalpha(V, A_KC_am, B_KC_am, C_KC_am)
-def bm_KC(V):   return KCbeta(V, A_KC_bm, B_KC_bm, C_KC_bm)
+def am_KC(V):   return KC_alpha(V, A_KC_am, B_KC_am, C_KC_am, #insert conc)
+def bm_KC(V):   return KC_beta(V, A_KC_bm, B_KC_bm, C_KC_bm)
 
 # define the currents in the model
 def I_NaF(V, m, h): return g_Na * (m**3) * h * (V - E_NaF)
@@ -86,8 +86,8 @@ def I_l(V): return g_l * (V - E_l)
 # define function that will return time derivatives for integration
 def f(t, y):
     # set external current: make sure it is 0 for large times.
-    if t <= 1:
-        I_e = 20
+    if 10 <= t <= 12:
+        I_e = 10
     else:
         I_e = 0
     # set the variables that are to be integrated
@@ -120,11 +120,11 @@ h_CaL_0 = 1 / (ah_CaL(V_0)  + bh_CaL(V_0))
 m_KA_0 = minf_KA(V_0)
 h_KA_0 = hinf_KA(V_0)
 m_KC_0 = 1 / (am_KC(V_0)  + bm_KC(V_0))
-y_0 = [V_0, n_0, m_0, h_0]
+y_0 = [V_0, m_NaF_0, h_NaF_0, m_Kdr_0, h_Kdr_0, m_CaL_0, h_CaL_0, m_KA_0, h_KA_0, m_KC_0]
 
 # create timescale. t_interval is the time interval in which to calculate the solution.
 # t_points are the points at which the solution is stored.
-t_interval = (0.0, 40.0)
+t_interval = (0.0, 50.0)
 numpoints = 1000
 t_points = np.linspace(t_interval[0], t_interval[1], numpoints)
 
@@ -132,27 +132,7 @@ t_points = np.linspace(t_interval[0], t_interval[1], numpoints)
 soln = sp_int.solve_ivp(f, t_interval, y_0, 'BDF')
 
 V = soln.y[0, :]
-n = soln.y[1, :]
-m = soln.y[2, :]
-h = soln.y[3, :]
 
 # plot & show solution
 plt.figure(1)
-plt.plot(soln.t, V, 'g') # a V, t plot
-f, (ax1, ax2, ax3) = plt.subplots(3, sharex = True)
-# plt.plot(soln.t, [I_Na(i, j, k) for i,j,k in zip(V, m, h)], 'blue', label='sodium current')
-# plt.plot(soln.t, [I_K(i, j) for i,j in zip(V, n)], 'red', label='potassium current')
-# plt.plot(soln.t, [I_l(i) for i in V], 'green', label='leak current')
-t_i = 0
-t_f = 50
-ax1.plot(soln.t[t_i:t_f], [I_Na(i,j,k) + I_K(i,l) + I_l(i) for i,j,k,l in zip(V[t_i:t_f],m[t_i:t_f],h[t_i:t_f],n[t_i:t_f])], 'g', label='Ion currents')
-ax1.set_ylabel('Current (uA)')
-ax1.set_xlabel('Time (ms)')
-ax2.plot(soln.t[t_i:t_f], m[t_i:t_f], 'b', label='m')
-ax2.plot(soln.t[t_i:t_f], h[t_i:t_f], 'y', label='h')
-ax2.plot(soln.t[t_i:t_f], n[t_i:t_f], 'r', label='n')
-ax2.set_ylabel('Probability')
-ax2.set_xlabel('Time (ms)')
-ax3.plot(soln.t[t_i:t_f], [-m_i + ((1 - h_i) + n_i) for m_i, h_i, n_i in zip(m[t_i:t_f], h[t_i:t_f], n[t_i:t_f])], label = 'inactivation - activation')
-plt.legend()
-plt.show()
+plt.plot(soln.t, V, 'b') # a V, t plot
